461ab105fa80782a3151b766aa7afd2434ab8bf7
QuantumScrewdriver <bsk945@gmail.com>
2019-07-21 09:02:27 -0700

    Updates

---
+++ precache-manifest.2d8c49f9d2f4012d6e958ed6fed1cf19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c20235f434538a55e7dfdf3eaa09d27b",
+    "url": "/test-comic/index.html"
+  },
+  {
+    "revision": "69cfd55aabc352ee8c02",
+    "url": "/test-comic/static/css/main.97d22e8b.chunk.css"
+  },
+  {
+    "revision": "a36533ae65938741f383",
+    "url": "/test-comic/static/js/2.4f017e5b.chunk.js"
+  },
+  {
+    "revision": "69cfd55aabc352ee8c02",
+    "url": "/test-comic/static/js/main.8f8286d6.chunk.js"
+  },
+  {
+    "revision": "1f926fb38f8fcb2f2ba1",
+    "url": "/test-comic/static/js/runtime~main.93af21ee.js"
+  }
+]);
